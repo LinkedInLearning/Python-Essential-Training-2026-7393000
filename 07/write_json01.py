4a343ce51e68bb5910aba7cb25bb2c4c8170ca30
kanehiro <kanehiro@gmail.com>
2026-03-17 10:30:27 +0900

    Fix file path for JSON output in write_json01.py

--- 07/write_json01.py
+++ 07/write_json01.py
@@ -1,6 +1,6 @@
 import json
 
-f = open("test1.json","w")
+f = open("07/test1.json","w")
 # json.dump({"11013": {"english":65,"math":77,"japanese":56,"science":65,"social":80}},f)
 json.dump({"11013": {"english":65,"math":77,"japanese":56,"science":65,"social":80}},f, indent=4)
 f.close()
