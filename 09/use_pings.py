898135bcc98fdab948cb6e2b9d9719f5448c6e10
kanehiro <kanehiro@gmail.com>
2026-03-23 11:22:03 +0900

    Update use_pings.py

--- 09/use_pings.py
+++ 09/use_pings.py
@@ -1,8 +1,8 @@
 import ping3
 ping3.EXCEPTIONS = True
 
-target_ip = "192.168.8.109"
-# target_ip = "192.168.8.125"
+target_ip = "127.0.0.1"
+# target_ip = "192.168.8.109"
 try:
     result = ping3.ping(target_ip, timeout = 0.5, unit ='ms', ttl = 64) 
     print(f'Host : {target_ip} is reachable : {result}(ms)')
